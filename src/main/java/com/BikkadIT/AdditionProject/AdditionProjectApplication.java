4a823ddc79c5f592bb7945a24282e8d6b92e2f6d
Moseemp <moseemp@gmail.com>
2023-04-22 23:58:07 +0530

    commit by Moseem 1st

---
+++ src/main/java/com/BikkadIT/AdditionProject/AdditionProjectApplication.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.AdditionProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdditionProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdditionProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BikkadIT/AdditionProject/AdditionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BikkadIT.AdditionProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdditionProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
